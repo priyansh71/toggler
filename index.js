a4f9e7290dbba861a5b9f365d8c9cc616698ff80
priyansh71 <f20201388@goa.bits-pilani.ac.in>
2021-06-06 11:58:39 +0530

    modified javascript features

--- index.js
+++ index.js
@@ -27,10 +27,20 @@ const toggler = () => {
 	document.getElementById("output").innerHTML = result;
 }
 
+// function toggle(){
+// 	document.getElementById("output").style.touchAction = "none";
+// }
+
 function copy() {
 	var copyText = document.querySelector("#output");
 	copyText.select();
 	document.execCommand("copy");
+	let a = document.getElementById("output").value;
+	if (a == null || a == " " || a.length == 0) {
+	
+		document.getElementById("toggle").innerText = "Please enter valid text!";
+		document.getElementById("toggle").style.color = "#110000";
+	}else {
+		document.getElementById("toggle").innerText = "Copying successful!";
+	}
 }
-
-// document.querySelector("#copy").addEventListener("click", copy);
